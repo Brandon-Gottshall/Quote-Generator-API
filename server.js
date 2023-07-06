5efcc5f35c3b76b9bdfb488ba400af86b3423733
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-07-06 00:22:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ app.get('/', async (req, res) => {
   }
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 

2813da51bd0865c180c44e36fc83bdcb8e054fef
Brandon🤖 <brandon@effortlessdevelopment.com>
2023-07-06 10:28:34 -0400

    Update PM2 script

--- server.js
+++ server.js
@@ -166,10 +166,113 @@
 // Insert a new quote
 // INSERT INTO quotes (quote) VALUES ('I am Batman');
 
-// Stage 4: Connect the API to the database
+// // Stage 4: Connect the API to the database
+// const express = require('express')
+// const pg = require('pg')
+// const router = express.Router()
+
+// // Database Setup
+// const { Pool } = pg // imports the Pool class from pg
+// const pool = new Pool({
+//   connectionString:
+//     'postgresql://postgres:jGIUrmoBCP8nP4k2xxfx@containers-us-west-167.railway.app:6254/railway',
+// }) // instantiates the Pool class
+
+// // Express Setup
+// const app = express()
+// const port = process.env.PORT || 3000
+// console.log('port', port)
+
+// // We need to add a middleware to express that will allow us to parse JSON from the request body
+// // This will allow us to get the quote from the request body
+// app.use(express.json())
+
+// router.get('/', async (req, res) => {
+//   // test the database connection
+//   try {
+//     const client = await pool.connect()
+//     const result = await client.query('SELECT * FROM quotes')
+//     const results = { results: result ? result.rows : null }
+//     res.send(results)
+//     client.release()
+//   } catch (err) {
+//     console.error(err)
+//     res.send('Error ' + err)
+//   }
+// })
+
+// // Get all favorited quotes
+// router.get('/quotes', async (req, res) => {
+//   try {
+//     const client = await pool.connect()
+//     let quotes = await client.query('SELECT * FROM quotes')
+//     client.release()
+//     res.send(quotes.rows)
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).send('Error ' + err)
+//   }
+// })
+
+// // Generate a new quote
+// //  Mock Quote - We will replace this with a real quote when we add OpenAI
+// router.get('/quotes/new', async (req, res) => {
+//   res.send({
+//     id: 1,
+//     quote: "I'm Batman",
+//   })
+// })
+
+// // Favorite a quote
+// // Mock Quote added
+// router.post('/quotes/favorite', async (req, res) => {
+//   // Here is where we will add the quote to the database
+//   let quote = req.body.quote
+
+//   try {
+//     const client = await pool.connect()
+//     let result = await client.query('INSERT INTO quotes (quote) VALUES ($1)', [
+//       quote,
+//     ])
+//     client.release()
+//     res.send({ quote })
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).send('Error ' + err)
+//   }
+// })
+
+// // Remove a quote from favorites
+// router.delete('/quotes/:id/favorite', async (req, res) => {
+//   // Here is where we will remove the quote from the database
+//   try {
+//     const client = await pool.connect()
+//     let result = await client.query('DELETE FROM quotes WHERE id = $1', [
+//       req.params.id,
+//     ])
+//     client.release()
+//     res.send('Removed: ', {
+//       id: req.params.id,
+//     })
+//   } catch (err) {
+//     console.log(err)
+//     res.status(500).send('Error ' + err)
+//   }
+// })
+
+// app.use('/', router)
+
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+// Stage 5: Add OpenAI
 const express = require('express')
 const pg = require('pg')
 const router = express.Router()
+
+// Get environment variables
+require('dotenv').config()
 
 // Database Setup
 const { Pool } = pg // imports the Pool class from pg
@@ -182,6 +285,14 @@ const pool = new Pool({
 const app = express()
 const port = process.env.PORT || 3000
 console.log('port', port)
+
+// OpenAI Setup
+const { OpenAIApi, Configuration } = require('openai')
+// openai.apiKey = process.env.OPEN_AI_API_KEY
+const config = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY,
+})
+const client = new OpenAIApi(config)
 
 // We need to add a middleware to express that will allow us to parse JSON from the request body
 // This will allow us to get the quote from the request body
@@ -216,11 +327,36 @@ router.get('/quotes', async (req, res) => {
 
 // Generate a new quote
 //  Mock Quote - We will replace this with a real quote when we add OpenAI
-router.get('/quotes/new', async (req, res) => {
-  res.send({
-    id: 1,
-    quote: "I'm Batman",
-  })
+router.get('/quotes/new/:topic', async (req, res) => {
+  let topic = req.params.topic
+  let prompt = `Create an original quote about ${topic}.`
+  // OpenAI Completion
+  let models
+  try {
+    // const gptResponse = await openai.Completion.create({
+    //   engine: 'davinci',
+    //   prompt: prompt,
+    //   maxTokens: 64,
+    // })
+    // console.log(gptResponse)
+    // const quote = gptResponse.data.choices[0].text.trim()
+    models = await client.listEngines().then(response => response.data.data)
+    quote = await client
+      .createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: prompt }],
+      })
+      .then((response, err) => {
+        if (err) {
+          console.log(err)
+        }
+        return response.data.choices[0].message.content
+      })
+    res.send(quote)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Error ' + err)
+  }
 })
 
 // Favorite a quote
